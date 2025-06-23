17bd00d1721387117af8dcba2cf55adfb9ac05aa
Santiago Garcia <102694038+Santy1422@users.noreply.github.com>
2025-06-23 20:21:10 +0200

    up

--- api/src/routes/clients.ts
+++ api/src/routes/clients.ts
@@ -7,21 +7,21 @@ const { catchedAsync } = require('../utils');
 const router = Router();
 
 // Crear cliente
-router.post('/', jwtUtils, catchedAsync(clientsControllers.createClient));
+router.post('/', catchedAsync(clientsControllers.createClient));
 
 // Obtener todos los clientes
-router.get('/', jwtUtils, catchedAsync(clientsControllers.getAllClients));
+router.get('/', catchedAsync(clientsControllers.getAllClients));
 
 // Obtener clientes activos
-router.get('/active', jwtUtils, catchedAsync(clientsControllers.getActiveClients));
+router.get('/active', catchedAsync(clientsControllers.getActiveClients));
 
 // Obtener cliente por ID
-router.get('/:id', jwtUtils, catchedAsync(clientsControllers.getClientById));
+router.get('/:id', catchedAsync(clientsControllers.getClientById));
 
 // Actualizar cliente
-router.put('/:id', jwtUtils, catchedAsync(clientsControllers.updateClient));
+router.put('/:id', catchedAsync(clientsControllers.updateClient));
 
 // Eliminar cliente
-router.delete('/:id', jwtUtils, catchedAsync(clientsControllers.deleteClient));
+router.delete('/:id', catchedAsync(clientsControllers.deleteClient));
 
 export default router;
